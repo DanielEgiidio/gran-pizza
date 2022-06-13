9e11cd43783d58e7db489bb4f062d8d0e35f6b83
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-06-13 17:42:54 -0300

    Finalização da Sessão de Video

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
-import images from './images';
-import meal from '../assets/meal.mp4';
-import data from './data';
+import images from "./images";
+import meal from "../assets/meal.mp4";
+import pizzameal from "../assets/pizzameal.mp4";
+import data from "./data";
 
-export { images, meal, data };
+export { images, meal, pizzameal, data };
